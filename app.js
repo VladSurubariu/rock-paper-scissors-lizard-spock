f5697c69f01a780cc8e3d44bc5e0d48263d37767
Vlad Surubariu <77098036+VladSurubariu@users.noreply.github.com>
2021-08-18 19:28:06 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var i=0;
+
+function pop(){
+    if(i==0){
+        document.getElementById("item1").style.transform='translate(-150px,-50px)'
+        document.getElementById("item2").style.transform='translate(-5px, -180px)'
+        document.getElementById("item3").style.transform='translate(150px, -50px)'
+        document.getElementById("item4").style.transform='translate(-150px, -50px)'
+        document.getElementById("item5").style.transform='translate(-150px, -50px)'
+        return
+    }
+}
